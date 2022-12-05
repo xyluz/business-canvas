54b269f68ae7616aac0bc58aa4987830e14c5f43
tunny17 <tvnji17@gmail.com>
2022-12-05 21:39:41 +0100

    completion of the input field logic

--- Frontend/src/components/SignIn/SignIn.jsx
+++ Frontend/src/components/SignIn/SignIn.jsx
@@ -1,8 +1,53 @@
 import React from 'react';
-import images from '../../constants/images';
 import styles from './SignIn.module.css';
+import { useFormik } from 'formik';
+
+
+const initialValues = {
+  name: '',
+  password: ''
+}
+
+const onSubmit = values => {
+  console.log('Form data', values)
+}
+
+const validate = values => {
+  let errors = {}
+
+  if(!values.name) {
+    errors.name = 'Please Input Full Name'
+  } 
+
+
+  // if (!values.email) {
+  //   errors.email = 'Please Input your email'
+  // } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+  //   errors.email = 'Invalid email format'
+  // }
+
+
+
+  if (!values.password) {
+    errors.password = 'Please Input Password'
+  }
+
+  return errors
+}
 
 const SignIn = () => {
+
+
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+    validate 
+  })
+
+
+  console.log('Visited fields', formik.touched)
+
+
   return (
     <div className={styles.bodyContainer}>
       <div  className={styles.left}>
@@ -21,15 +66,37 @@ const SignIn = () => {
             <p>-------- Or --------</p>
           </div>
           <div className={styles.form}>
-            <form action="">
+            <form onSubmit={formik.handleSubmit}>
               <label htmlFor="full-name"> Enter Full Name or Email <br />
-                <input type="text" name="full-name" id="text" />
+                <input 
+                  type="text" 
+                  id="name" 
+                  onChange={formik.handleChange} 
+                  onBlur={formik.handleBlur}
+                  value={formik.values.username}
+                />
               </label> <br />
+              {formik.touched.name && formik.errors.name ? (
+                <p  className={styles.error}>{formik.errors.name}</p>
+              ) : null}
+
               <br />
+
               <label htmlFor="full-name"> Enter Password <br />
-                <input type="text" name="full-name" id="text" />
+                <input 
+                  type="text" 
+                  id="password" 
+                  onChange={formik.handleChange} 
+                  onBlur={formik.handleBlur}
+                  value={formik.values.username}
+                />
               </label>
               <br />
+              {formik.touched.password && formik.errors.password ? (
+                <p  className={styles.error}>{formik.errors.password}</p>
+              ) : null}
+
+
               <div  className={styles.checkboxes}>            
                 <label for="passwordCharacters"> Password must have at least 8 characters 
                   <input type="checkbox" name="" id="passwordCharacters" />
@@ -43,7 +110,7 @@ const SignIn = () => {
                   <input type="checkbox" name="" id="passwordCasing" />
                 </label>
               </div>
-              <button>Create an account</button>
+              <button type='submit'>Create an account</button>
             </form>
           </div>
           <footer> Don’t have an account? <span>Sign up</span> </footer>
