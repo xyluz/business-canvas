c7074f49ef2ac11eb5a2051e869021e66bdb6461
tunny17 <tvnji17@gmail.com>
2022-12-05 09:12:25 +0100

    - styling of the right section (form) - replaced the classNames in the SignIn.js file - removed the border-box property

--- Frontend/src/components/SignIn/SignIn.jsx
+++ Frontend/src/components/SignIn/SignIn.jsx
@@ -14,30 +14,32 @@ const SignIn = () => {
         </div>
       </div>
       <div  className={styles.right}>
-        <div  className='form-text'>
-          <h2>Sign In</h2>
-          <button type='button' className='signIn-button'>Sign in with google</button>
-          <p>-------- Or --------</p>
+        <div className={styles.rightContainer}>
+          <div  className={styles.formText}>
+            <h2>Sign In</h2>
+            <button type='button' className='signIn-button'>Sign in with google</button>
+            <p>-------- Or --------</p>
+          </div>
+          <div className='form'>
+            <form action="">
+              <label htmlFor="full-name"> Enter Full Name or Email <br />
+                <input type="text" name="full-name" id="text" />
+              </label> <br />
+              <label htmlFor="full-name"> Enter Password <br />
+                <input type="text" name="full-name" id="text" />
+              </label>
+              <div  className='checkboxes'>            
+                <input type="checkbox" name="" id="" />Password must have at least 8 characters <br />
+                <input type="checkbox" name="" id="" />Password must have at least 1 digit <br />
+                <input type="checkbox" name="" id="" />Password must have Uppercase character  <br />
+              </div>
+              <button>Create an account</button>
+            </form>
+          </div>
+          <footer>
+            <p>Don’t have an account? <span>Sign up</span></p>
+          </footer>
         </div>
-        <div className='form'>
-          <form action="">
-            <label htmlFor="full-name"> Enter Full Name or Email
-              <input type="text" name="full-name" id="text" />
-            </label>
-            <label htmlFor="full-name"> Enter Password
-              <input type="text" name="full-name" id="text" />
-            </label>
-            <div  className='checkboxes'>            
-              <input type="checkbox" name="" id="" />Password must have at least 8 characters <br />
-              <input type="checkbox" name="" id="" />Password must have at least 1 digit <br />
-              <input type="checkbox" name="" id="" />Password must have Uppercase character  <br />
-            </div>
-            <button>Create an account</button>
-          </form>
-        </div>
-        <footer>
-          <p>Don’t have an account? <span>Sign up</span></p>
-        </footer>
       </div>
     </div>
   )
